a21a18bae12566b8c9cdf823548add28d95e2110
ltlanc4 <ltlanc4@gmail.com>
2022-02-07 16:02:06 +0700

    Lai lan nua

--- app.js
+++ app.js
@@ -15,18 +15,26 @@ const App = (function () {
             event.preventDefault();
             if (event.target.classList.contains("btn-yes") || event.target.classList.contains("btn-yes-du-phong")) {
                 document.querySelector(".text-success").classList.remove("d-none");
+                document.querySelector("#reset").classList.remove("d-none");
             }
             if (event.target.classList.contains("btn-no")) {
                 document.querySelector('#ban-dau').classList.add("d-none");
                 document.querySelector('#du-phong').classList.remove("d-none");
-                document.querySelector(".text-success").classList.remove("d-none");
             }
             if (event.target.classList.contains("btn-no-du-phong")) {
                 document.querySelector('#ban-dau').classList.remove("d-none");
                 document.querySelector('#du-phong').classList.add("d-none");
-                document.querySelector(".text-success").classList.remove("d-none");
             }
-        })
+        });
+        document.querySelector("body").addEventListener('click', event => {
+            event.preventDefault();
+            if (event.target.classList.contains("btn-reset")) {
+                document.querySelector(".text-success").classList.add("d-none");
+                document.querySelector('#ban-dau').classList.remove("d-none");
+                document.querySelector('#du-phong').classList.add("d-none");
+                document.querySelector("#reset").classList.add("d-none");
+            }
+        });
     }
     return {
         init,
